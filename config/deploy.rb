9b66904c7b451b0e80cda52e5b226eb5e8eaeccd
m.gerecke <mgerecke@web.de>
2014-08-01 21:58:31 +0200

    before deploy 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,60 +1,119 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+###############################
+#
+# Capistrano Deployment on shared Webhosting by RailsHoster
+#
+# maintained by support@railshoster.de
+#
+###############################
 
-set :application, 'enex'
-set :repo_url, 'mgerecke@github.com/enex.git'
+def gemfile_exists? 
+  File.exists? "Gemfile"
+end
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+def gemfile_lock_exists?
+  File.exists? "Gemfile.lock"
+end
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+def rails_version
+  if gemfile_exists? 
+    rails_v = (Proc.new {
+      file = File.new("Gemfile", "r")
+    while (line = file.gets)
+        if line =~ /^\s*gem\s+[\"\']rails[\"\'].+/
+          version = line.scan(/(\d+)\.(\d+)\.(\d+)/).first.map {|x| x.to_i}
+        end
+      end
+      file.close
+      version
+    }).call
+  else 
+    rails_v = nil
+  end
+  return rails_v
+end
 
-# Default value for :scm is :git
-# set :scm, :git
+def rails_version_supports_assets?
+  rails_v = rails_version
+  return rails_v != nil && rails_v[0] >= 3 && rails_v[1] >= 1
+end
 
-# Default value for :format is :pretty
-# set :format, :pretty
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+#### Use the asset-pipeline and setup bulder
+require 'capistrano/bundler'
 
-# Default value for :pty is false
-# set :pty, true
+set :bundle_roles, :all   
+set :bundle_servers, -> { release_roles(fetch(:bundle_roles)) }
+set :bundle_binstubs, -> { shared_path.join('bin') } 
+set :bundle_without, %w{development test}.join(' ') 
+set :bundle_path, -> { shared_path.join('bundle') }
+set :bundle_flags, '--deployment --quiet'
+set :bundle_bins, %w{gem rake rails}
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+require 'capistrano/rails/assets' if rails_version_supports_assets?
 
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+#### Personal Settings
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# application name ( should be rails1 rails2 rails3 ... )
+set :application, "rails1"
+set :stage, :development
 
-set :stages, ["development"]
+# repository location
+set :repo_url, "mgerecke@github.com/enex.git"
+
+# :subversionn or :git
+set :scm, :git
+set :scm_verbose, true
+
+server("rho.railshoster.de", {
+  :user => "user01862291",
+  :roles => [:web, :app, :db],
+  :ssh_options => {
+    :forward_agent => true,
+    :auth_methods  => %w{publickey password},
+    :password      => "EgTmzWVPNk"
+  }
+})
+
+# set the location where to deploy the new project
+set :deploy_to, "/home/user01862291/#{fetch(:application)}"
+
+
+set :assets_roles,   :app
+set :assets_prefix, 'assets'
+
+set :linked_dirs, fetch(:linked_dirs, []).push("public/#{fetch(:assets_prefix)}") 
+set :migration_role, :db
+
+# change the environment if needed
+set :rails_env, :production
+
+# enable migration tasks if the deploy.rb was used into a rails app
+require 'capistrano/rails/migrations' if rails_version
+
+
+# don't use sudo it's not necessary
+set :use_sudo, false
+
+
+
+############################################
+# Default Tasks by RailsHoster.de
+############################################
 
 namespace :deploy do
-
-  desc 'Restart application'
+  desc "Restarting mod_rails with restart.txt"
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    on roles(:app), :in => :sequence, :wait => 5 do
+      execute "touch #{fetch(:current_path)}/tmp/restart.txt"
     end
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Additional Symlinks ( database.yml, etc. )"
+  before :migrate, :additional_symlink do
+    on roles(:app) do
+      execute "ln -fs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     end
   end
-
 end
+
